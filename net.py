5c3f936476c70d2c2a4aa3b511ba5903339bb533
Angelica <67800009+alstar555@users.noreply.github.com>
2022-08-01 09:29:01 -0400

    Update net.py
    added backprop

--- net.py
+++ net.py
@@ -15,7 +15,6 @@ def print_net(layers, num_layers):
     print()
 
 def forward_propogation(num_layers):
-    print("starting forward propogation ...")
     for i in range(num_layers-1):
         w = weights[i]
         x = layers[i]
@@ -27,20 +26,26 @@ def forward_propogation(num_layers):
 def sigmoid(z):
     return 1 /(1 + np.exp(-z))
  
-def back_propogation(error): #d out y1 = y1  dy1 = dy2
-    dE_dY1 = - np.subtract(expected_output, layers[-1])
-    dY1_dY2 = 0
-    dY2_dW = 0
-    dE_dW = dE_dY1 * dY1_dY2 * dY2_dW
-    update_weights(dE_dW)
-    return 0
+def back_propogation(cost_gradient, i): 
+    print(cost_gradient)
+    print(layers[i])
+    weight_gradient = np.dot(cost_gradient, np.transpose(layers[i]))
+    update_weights(weight_gradient, i)
+    bias_gradient = cost_gradient
+    update_biases(bias_gradient, i)
+    return np.dot(np.transpose(weights[i]), cost_gradient)
 
-def update_weights(dE_dW):
-    dE_dW = np.dot(LEARNING_RATE, dE_dW)
-    layers = np.subtract(layers, dE_dW)
+def update_weights(gradient, i):
+    gradient = np.dot(LEARNING_RATE, gradient)
+    weights[i] = np.subtract(weights[i], gradient)
+
+def update_biases(gradient, i):
+    gradient = np.dot(LEARNING_RATE, gradient)
+    biases[i] = np.subtract(biases[i], gradient)
+
 
 def cost_function(expected_output):
-    return np.sum(1/2 * (layers[-1] - expected_output) ** 2)
+    return np.power(np.subtract(layers[-1], expected_output),1)
 
 def print_results():
     print(layers[0], "  =  ", layers[-1])
@@ -56,40 +61,57 @@ def count_num_neurons():
 #XOR data
 training_inputs = [[1,0], [1, 1], [0, 0]]
 training_outputs = [[1],[0],[0]]
+
 testing_input = [0,1]
 testing_output = [1]
 
 input_layer = training_inputs[0]
 output_layer = training_outputs[0]
 
-# for i in range(len(training_inputs)):
-#     input_layer = training_inputs[i]
-#     expected_output = training_outputs[i]
-#     layers[0] = input_layer
-#     forward_propogation()
-#     cost += cost_function(expected_output)
-#     print_results()
-# back_propogation()
 
-input_layer = [0.05, 0.10]
+# input_layer = [0.05, 0.10]
 weights = [[[0.15, 0.20], [0.25, 0.30]], 
-           [[0.40, 0.45], [0.50, 0.55]]]
+           [[0.40, 0.45]]]
 biases = [0.35, 0.60]
-expected_output = [0.01, 0.99]
-layers = []
+# expected_output = [0.01, 0.99]
+
+
+#training
+for k in range(len(training_inputs)):
+    layers = []
+    hidden_layers = [[0, 0]]
+    output_layer = [0]
+
+    input_layer = training_inputs[k]
+    expected_output = training_outputs[k]
+
+    layers.append(input_layer)
+    for l in hidden_layers:
+        layers.append(l)
+    layers.append(output_layer)
+
+    num_neurons = count_num_neurons()
+    num_layers = len(layers)
+    for x in range(2):
+        forward_propogation(num_layers)
+        cost_gradient = cost_function(expected_output)
+        for i in range(num_layers-2, -1, -1):
+            cost_gradient = back_propogation(cost_gradient, i)
+
+    
+
+
+
+#testing
+input_layer = testing_input
+expected_output = testing_output
 hidden_layers = [[0, 0]]
-output_layer = [0, 0]
+output_layer = [0]
+layers = []
 layers.append(input_layer)
 for l in hidden_layers:
     layers.append(l)
 layers.append(output_layer)
-
-num_neurons = count_num_neurons()
-num_layers = len(layers)
-
 forward_propogation(num_layers)
 print_net(layers, num_layers)
-error = cost_function(expected_output)
-back_propogation(error)
-print_results()
 
